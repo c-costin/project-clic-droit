166d1f7f774adf2353284fe75f038908bd5d4ab1
c-costin <contact@costincadeau.fr>
2023-12-27 22:35:42 +0100

    build: added Slim router and create app

---
+++ app/src/Model/CoreModel.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+abstract class CoreModel
+{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $created_at;
+    /**
+     * @var string
+     */
+    protected $updated_at;
+
+    abstract public function insert();
+    abstract public static function find(int $id);
+    abstract public static function findAll();
+    abstract public function update();
+    abstract public function delete();
+
+    /**
+     * save method
+     *
+     * @return bool
+     */
+    public function save()
+    {
+        if ($this->id === null) {
+            return $this->insert();
+        } else {
+            return $this->update();
+        }
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return  int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of created_at
+     *
+     * @return  string
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Get the value of updated_at
+     *
+     * @return  string
+     */
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+}
